f27e15673a2b85bbdd35f3a4b3e58637a8731bdf
Charles Jackson <cpjackson572@gmail.com>
2019-10-09 14:44:35 -0500

    completed lightning exercises

---
+++ src/scripts/main.js
@@ -0,0 +1,38 @@
+console.log("We're learning about objects.")
+
+let car = {
+    make: "Ford",
+    model: "Mustang",
+    year: 2015,
+    color: "red"
+}
+
+let animalNames = ["Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"];
+
+const parentOne = {
+    name: "Bob",
+    gender: "male",
+    age: 82
+}
+
+const parentTwo = {
+    name: "Chad",
+    gender: "Male",
+    age: 100
+}
+
+const parentThree = {
+    name: "Chuck",
+    gender: "male",
+    age: 1
+}
+
+const child = {
+    name: "Sarah",
+    gender: "female",
+    age: 40
+}
+
+familyMembers = [parentOne, parentTwo, parentThree, child]
+
+console.log(familyMembers);
